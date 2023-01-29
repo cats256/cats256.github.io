6c10bbbcde14301da333b46c3e44327c91189921
cats256 <59489624+cats256@users.noreply.github.com>
2023-01-29 11:00:37 -0600

    Add files via upload

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ async function login() {
     document.getElementById("login-page").className = "hidden";
     document.getElementById("app-contents").className = "visible";
     document.getElementById("dashboard").className = "visible";
-    document.body.style.backgroundColor = "#f2f2f2";
+    document.body.style.backgroundColor = "#EEECDD";
   } 
   else {
     document.getElementById("error-message").className = "visible";
